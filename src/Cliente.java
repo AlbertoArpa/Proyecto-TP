5d37ea7c020b86cc7af43a3662ae7b7b2aee95b6
AlbertoArpa <a.arpa@alumnos.upm.es>
2023-11-30 12:41:41 +0100

    1

--- src/Cliente.java
+++ src/Cliente.java
@@ -41,12 +41,12 @@ public class Cliente {
 
     // TODO: Texto que debe generar: Zapp Brannigan, zapp.brannigan@dop.gov
     public String toString() {
-
+        return nombre + " " + apellidos + ", " + email;
     }
 
     // TODO: Devuelve un booleano que indica si se ha alcanzado el número máximo de envíos
     public boolean maxEnviosAlcanzado() {
-
+        return listaEnvios.estaLlena();
     }
 
     // TODO: Devuelve un envío en función de su posición
