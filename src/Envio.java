4654e5e13f55288c2a676552d7cc591612c9bdbd
AlbertoArpa <a.arpa@alumnos.upm.es>
2023-12-01 21:58:54 +0100

    todo menos ultimo metodo

--- src/Envio.java
+++ src/Envio.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author
  * @author
- * @version     1.0
+ * @version 1.0
  */
 public class Envio {
 
@@ -37,32 +37,42 @@ public class Envio {
         this.columna = columna;
         this.precio = precio;
     }
+
     public String getLocalizador() {
         return localizador;
     }
+
     public Porte getPorte() {
         return porte;
     }
+
     public Cliente getCliente() {
         return cliente;
     }
+
     public int getFila() {
         return fila;
     }
+
     public int getColumna() {
         return columna;
     }
+
     // TODO: Ejemplos: "1A" para el hueco con fila 1 y columna 1, "3D" para el hueco con fila 3 y columna 4
     public String getHueco() {
         return fila + "" + (((char) columna) + 'A' - 1);
     }
+
     public double getPrecio() {
         return precio;
     }
+
     //TODO: Texto que debe generar: Envío PM1111AAAABBBBC para Porte PM0066 de GGT M5 (01/01/2023 08:15:00) a CID M1 (01/01/2024 11:00:05) en hueco 6C por 13424,56 SSD
     public String toString() {
-        return "Envío " + localizador + " para Porte " + porte.getID() + " de " + porte.getOrigen().toStringSimple() + " (" + porte.getSalida() + ") a " + porte.getDestino().toStringSimple() + " (" + porte.getDestino() + ") en hueco " + getHueco() + " por " + precio + " SSD";
+        return "Envío " + localizador + " para Porte " + porte.getID() + " de " + porte.getOrigen().toStringSimple() + " M" + porte.getMuelleOrigen() + " (" + porte.getSalida() + ") a " +
+                porte.getDestino().toStringSimple() + " M" + porte.getMuelleDestino() + " (" + porte.getDestino() + ") en hueco " + getHueco() + " por " + precio + " SSD";
     }
+
     // TODO: Cancela este envío, eliminándolo de la lista de envíos del porte y del cliente correspondiente
     public boolean cancelar() {
         porte.getListaEnvios().eliminarEnvio(localizador);
@@ -72,51 +82,54 @@ public class Envio {
     /**
      * TODO: Método para imprimir la información de este envío en un fichero que respecta el formato descrito en el
      *  enunciado
+     *
      * @param fichero
      * @return Devuelve la información con el siguiente formato como ejemplo ->
-     *     -----------------------------------------------------
-     *     --------- Factura del envío PM1111AAAABBBBC ---------
-     *     -----------------------------------------------------
-     *     Porte: PM0066
-     *     Origen: Gaia Galactic Terminal (GGT) M5
-     *     Destino: Cidonia (CID) M1
-     *     Salida: 01/01/2023 08:15:00
-     *     Llegada: 01/01/2024 11:00:05
-     *     Cliente: Zapp Brannigan, zapp.brannigan@dop.gov
-     *     Hueco: 6C
-     *     Precio: 13424,56 SSD
+     * -----------------------------------------------------
+     * --------- Factura del envío PM1111AAAABBBBC ---------
+     * -----------------------------------------------------
+     * Porte: PM0066
+     * Origen: Gaia Galactic Terminal (GGT) M5
+     * Destino: Cidonia (CID) M1
+     * Salida: 01/01/2023 08:15:00
+     * Llegada: 01/01/2024 11:00:05
+     * Cliente: Zapp Brannigan, zapp.brannigan@dop.gov
+     * Hueco: 6C
+     * Precio: 13424,56 SSD
      */
     public boolean generarFactura(String fichero) {
         try {
-
-
-
-
-
-
-
-
+            PrintWriter salida = new PrintWriter(fichero);
+            salida.print("-----------------------------------------------------\n--------- Factura del envío " +
+                    localizador + " ---------\n-----------------------------------------------------\nPorte: " + porte.getID() +
+                    "\nOrigen: " + porte.getOrigen().toStringSimple() + " M" + porte.getMuelleOrigen() +
+                    "\nDestino: " + porte.getDestino().toStringSimple() + " M" + porte.getMuelleDestino() +
+                    "\nSalida: " + porte.getSalida() +
+                    "\nLlegada: " + porte.getLlegada() +
+                    "\nCliente: " + cliente.toString() +
+                    "\nHueco: " + getHueco() +
+                    "\nPrecio: " + precio + " SSD");
+            salida.close();
             return true;
         } catch (FileNotFoundException e) {
             return false;
         }
     }
 
-
-
     /**
-     *	TODO: Genera un localizador de envío. Este consistirá en una cadena de 15 caracteres, de los cuales los seis
-	 *   primeros será el ID del porte asociado y los 9 siguientes serán letras mayúsculas aleatorias. Ejemplo: PM0123ABCD
+     * TODO: Genera un localizador de envío. Este consistirá en una cadena de 15 caracteres, de los cuales los seis
+     *   primeros será el ID del porte asociado y los 9 siguientes serán letras mayúsculas aleatorias. Ejemplo: PM0123ABCD
      *   NOTA: Usar el objeto rand pasado como argumento para la parte aleatoria.
+     *
      * @param rand
      * @param idPorte
      * @return
      */
     public static String generarLocalizador(Random rand, String idPorte) {
         StringBuilder localizador = new StringBuilder(idPorte);
-
-
-
+        for (int i = 1; i <= 9; i++) {
+            localizador.append((char) (rand.nextInt('A' - 'Z' + 1) + 'A'));
+        }
         return localizador.toString();
     }
 
@@ -125,6 +138,7 @@ public class Envio {
      * TODO: Método para crear un nuevo envío para un porte y cliente específico, pidiendo por teclado los datos
      *  necesarios al usuario en el orden y con los textos indicados en los ejemplos de ejecución del enunciado.
      *  La función solicita repetidamente los parámetros hasta que sean correctos
+     *
      * @param teclado
      * @param rand
      * @param porte
@@ -132,9 +146,8 @@ public class Envio {
      * @return Envio para el porte y cliente especificados
      */
     public static Envio altaEnvio(Scanner teclado, Random rand, Porte porte, Cliente cliente) {
+        System.out.println("");
 
-
-
-        return ;
+        return;
     }
 }
