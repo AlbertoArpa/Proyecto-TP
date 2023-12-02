bead94b67199413b89c333d4be441d7fe620da9d
AlbertoArpa <a.arpa@alumnos.upm.es>
2023-12-02 22:26:57 +0100

    COMPLETA

--- src/ListaPuertosEspaciales.java
+++ src/ListaPuertosEspaciales.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author
  * @author
- * @version     1.0
+ * @version 1.0
  */
 public class ListaPuertosEspaciales {
     private PuertoEspacial[] lista;
@@ -17,66 +17,88 @@ public class ListaPuertosEspaciales {
      * @param capacidad
      */
     public ListaPuertosEspaciales(int capacidad) {
-        
-		
+
+
     }
+
     // TODO: Devuelve el número de puertos espaciales que hay en la lista
     public int getOcupacion() {
-
+        int i = 0;
+        while (lista[i] != null && i < lista.length - 1) i++;
+        return i;
     }
+
     // TODO: ¿Está llena la lista?
     public boolean estaLlena() {
-
+        return lista[lista.length - 1] != null;
     }
-	// TODO: Devuelve un puerto espacial dado un indice
+
+    // TODO: Devuelve un puerto espacial dado un indice
     public PuertoEspacial getPuertoEspacial(int i) {
-        return null;
+        return lista[i];
     }
 
     /**
      * TODO: insertamos un Puerto espacial nuevo en la lista
+     *
      * @param puertoEspacial
      * @return true en caso de que se añada correctamente, false en caso de lista llena o error
      */
     public boolean insertarPuertoEspacial(PuertoEspacial puertoEspacial) {
-
-        return false;
+        boolean result = false;
+        int i = 0;
+        while (lista[i] != null) i++;
+        if (lista[i] != null) {
+            lista[i] = puertoEspacial;
+            result = true;
+        }
+        return result;
     }
 
     /**
      * TODO: Buscamos un Puerto espacial a partir del codigo pasado como parámetro
+     *
      * @param codigo
      * @return Puerto espacial que encontramos o null si no existe
      */
     public PuertoEspacial buscarPuertoEspacial(String codigo) {
-
-        return null;
+        PuertoEspacial result = null;
+        int i = 0;
+        while (lista[i].getCodigo() != codigo) i++;
+        if (lista[i].getCodigo() == codigo) result = lista[i];
+        return result;
     }
 
     /**
      * TODO: Permite seleccionar un puerto espacial existente a partir de su código, usando el mensaje pasado como
      *  argumento para la solicitud y siguiendo el orden y los textos mostrados en el enunciado.
      *  La función solicita repetidamente el código hasta que se introduzca uno correcto
+     *
      * @param teclado
      * @param mensaje
      * @return
      */
     public PuertoEspacial seleccionarPuertoEspacial(Scanner teclado, String mensaje) {
         PuertoEspacial puertoEspacial = null;
-
-
+        String codigo = Utilidades.leerCadena(teclado, mensaje);
+        while (buscarPuertoEspacial(codigo).getCodigo() != codigo) {
+            System.out.println("\tCódigo de puerto no encontrado.");
+            codigo = Utilidades.leerCadena(teclado, mensaje);
+        }
+        puertoEspacial = buscarPuertoEspacial(codigo);
         return puertoEspacial;
     }
 
     /**
      * TODO: Genera un fichero CSV con la lista de puertos espaciales, SOBREESCRIBIENDOLO
+     *
      * @param nombre
      * @return
      */
     public boolean escribirPuertosEspacialesCsv(String nombre) {
         PrintWriter pw = null;
         try {
-
+            pw.
             return true;
         } catch (Exception e) {
             return false;
@@ -89,6 +111,7 @@ public class ListaPuertosEspaciales {
     /**
      * TODO: Genera una lista de PuertosEspaciales a partir del fichero CSV, usando el argumento como capacidad máxima
      *  de la lista
+     *
      * @param fichero
      * @param capacidad
      * @return
